ddb224db566a00b11881d6b16fce8ce20010c3b2
BitWire <thomas.schmidt@deskhelper.de>
2021-01-18 22:40:51 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Reddit Wallpaper Script for Linux
+
+Tested on Ubuntu 20.04 LTS
+
+## Dependencies
+
+- [Superpaper](https://github.com/hhannine/superpaper)
+- [Requests](https://requests.readthedocs.io/en/master/)
+
+## Config
+
+Config is simple.
+Put in the subreddit you want to query, the folder you want them to be saved to and how many Displays you have.
+For changing the wallpapers in intervals, use cron with the provided bash script
---
+++ wallpaper.py
@@ -0,0 +1,43 @@
+import requests, random, shutil, subprocess
+from configparser import ConfigParser
+from os.path import expanduser
+from pathlib import Path
+
+# Get configfile
+config_object = ConfigParser()
+config_object.read("config.ini")
+config = config_object["CONFIG"]
+
+# Get the data link of the subreddit
+link='https://www.reddit.com/r/'+ config['subreddit'] +'/.json'
+
+# Get the Path to save the pictures we will download, if it's not
+# there, it will be made
+path= expanduser("~") + '/Pictures/' + config['foldername'] + '/'
+Path(path).mkdir(parents=True, exist_ok=True)
+
+# Make the actual request to get the data
+r = requests.get(link, headers = {'User-agent': 'wallpaperscript 0.1'})
+
+# Get the json data from the request
+json = r.json()
+paths = []
+
+# Get a picture for every monitor
+for i in range(0,config['monitors']):
+    item = random.randrange(0,24)
+    post = json['data']['children'][item]['data']
+    if post['post_hint'] != 'image':
+        i-=1
+        break
+    image = requests.get(post['url'], stream=True)
+    if image.status_code == 200:
+        with open(path + 'pic_' + str(i), 'wb') as f:
+            image.raw.decode_content = True
+            shutil.copyfileobj(image.raw, f)
+    paths.append(path + 'pic_' + str(i))
+
+# Call superpaper with the pictures
+call = ['superpaper','-s'] + paths
+subprocess.check_call(call)
+
